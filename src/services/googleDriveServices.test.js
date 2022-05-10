cf5a3df0357c5a55c3b0a81f7503f53e881f1d4e
Lubomir Stavrev <62726686+Lubomir-Stavrev@users.noreply.github.com>
2022-05-10 16:57:46 +0300

    minimizing the code complexity in the tests

--- src/services/googleDriveServices.test.js
+++ src/services/googleDriveServices.test.js
@@ -13,53 +13,82 @@ jest.mock('react-native-encrypted-storage', () => {
 });
 
 describe('testing google drive services syncData()', () => {
+  const qrCodeValueThatShouldBeAdded = 'c';
+  beforeEach(() => {
+    jest.spyOn(googleDriveServices, 'getFolderId').mockImplementation(() => {});
+  });
+
   it('shouldnt add anything because the data is already synced', async () => {
-    jest.spyOn(googleDriveServices, 'getFolderId').mockReturnValue('1');
-    jest
-      .spyOn(googleDriveServices, 'getFilesWithContant')
-      .mockReturnValue([{content: 'a'}, {content: 'b'}, {content: 'c'}]);
-    jest.spyOn(encryptedStorageServices, 'getQRCodes').mockReturnValue([
+    const returnValueForEStQRCodesData = [
       {qrCodeData: 'a', id: '1'},
       {qrCodeData: 'b', id: '2'},
       {qrCodeData: 'c', id: '3'},
-    ]);
+    ];
+    const returnValueForDriveQRCodesData = [
+      {content: 'a'},
+      {content: 'b'},
+      {content: 'c'},
+    ];
+
+    jest
+      .spyOn(googleDriveServices, 'getFilesWithContant')
+      .mockReturnValue(returnValueForDriveQRCodesData);
+    jest
+      .spyOn(encryptedStorageServices, 'getQRCodes')
+      .mockReturnValue(returnValueForEStQRCodesData);
     jest.spyOn(googleDriveServices, 'addQRCode').mockImplementation(() => {});
 
     const driveAddQRCodeSpy = jest.spyOn(googleDriveServices, 'addQRCode');
     const ESAddQRCodeSpy = jest.spyOn(encryptedStorageServices, 'addQRCode');
+
     await googleDriveServices.syncDataWithDrive();
 
     expect(driveAddQRCodeSpy).not.toBeCalled();
     expect(ESAddQRCodeSpy).not.toBeCalled();
   });
   it('should add the drive data that is missing in the encrypted storage', async () => {
-    jest.spyOn(googleDriveServices, 'getFolderId').mockReturnValue('1');
-    jest
-      .spyOn(googleDriveServices, 'getFilesWithContant')
-      .mockReturnValue([{content: 'a'}, {content: 'b'}, {content: 'c'}]);
-    jest.spyOn(encryptedStorageServices, 'getQRCodes').mockReturnValue([
+    const returnValueForEStQRCodesData = [
       {qrCodeData: 'a', id: '1'},
       {qrCodeData: 'b', id: '2'},
-    ]);
-    const spy = jest.spyOn(encryptedStorageServices, 'addQRCode');
-    await googleDriveServices.syncDataWithDrive();
+    ];
+    const returnValueForDriveQRCodesData = [
+      {content: 'a'},
+      {content: 'b'},
+      {content: 'c'},
+    ];
 
-    expect(spy).toBeCalledWith('c');
-  });
-  it('should add the the encrypted storage data that is missing in the drive', async () => {
-    jest.spyOn(googleDriveServices, 'getFolderId').mockReturnValue('1');
     jest
       .spyOn(googleDriveServices, 'getFilesWithContant')
-      .mockReturnValue([{content: 'a'}, {content: 'b'}]);
-    jest.spyOn(encryptedStorageServices, 'getQRCodes').mockReturnValue([
+      .mockReturnValue(returnValueForDriveQRCodesData);
+    jest
+      .spyOn(encryptedStorageServices, 'getQRCodes')
+      .mockReturnValue(returnValueForEStQRCodesData);
+
+    const spy = jest.spyOn(encryptedStorageServices, 'addQRCode');
+
+    await googleDriveServices.syncDataWithDrive();
+
+    expect(spy).toBeCalledWith(qrCodeValueThatShouldBeAdded);
+  });
+  it('should add the the encrypted storage data that is missing in the drive', async () => {
+    const returnValueForEStQRCodesData = [
       {qrCodeData: 'a', id: '1'},
       {qrCodeData: 'b', id: '2'},
       {qrCodeData: 'c', id: '3'},
-    ]);
+    ];
+    const returnValueForDriveQRCodesData = [{content: 'a'}, {content: 'b'}];
+
+    jest
+      .spyOn(googleDriveServices, 'getFilesWithContant')
+      .mockReturnValue(returnValueForDriveQRCodesData);
+    jest
+      .spyOn(encryptedStorageServices, 'getQRCodes')
+      .mockReturnValue(returnValueForEStQRCodesData);
     jest.spyOn(googleDriveServices, 'addQRCode').mockImplementation(() => {});
+
     const spy = jest.spyOn(googleDriveServices, 'addQRCode');
     await googleDriveServices.syncDataWithDrive();
 
-    expect(spy).toBeCalledWith('c');
+    expect(spy).toBeCalledWith(qrCodeValueThatShouldBeAdded);
   });
 });
